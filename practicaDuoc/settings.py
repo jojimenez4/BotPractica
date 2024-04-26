9d30f3d329fd49b7f47626e3799d7f1e42481265
jojimenez4 <jo.jimenezh@duocuc.cl>
2024-04-26 15:59:04 -0400

    Arreglo BD y proyecto levantado

--- practicaDuoc/settings.py
+++ practicaDuoc/settings.py
@@ -86,12 +86,12 @@ WSGI_APPLICATION = "practicaDuoc.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "NAME": os.getenv('DB_NAME'),
-        "USER": os.getenv('DB_USER'),
-        "PASSWORD": os.getenv('DB_PASSWORD'),
-        "HOST": os.getenv('DB_HOST'),
-        "PORT": os.getenv('DB_PORT'),
+        'ENGINE': 'django.db.backends.mysql', 
+        'NAME': 'botpractica',
+        'USER': 'root',
+        'PASSWORD': 'system',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
